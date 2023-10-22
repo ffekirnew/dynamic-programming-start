b629daf93243259f8b153cab3251226eade41f3d
ffekirnew <ffekirnew0808@gmail.com>
2023-10-22 18:49:03 +0300

    Add black formatter to project
    No description

--- longest_increasing_subsequence/solution.py
+++ longest_increasing_subsequence/solution.py
@@ -9,10 +9,13 @@ class LongestIncreasingSubsequence:
         for index in range(len(sequence)):
             for other_number_index in range(0, index):
                 if sequence[index] > sequence[other_number_index]:
-                    if longest_ending_at[other_number_index] + 1 > longest_ending_at[index]:
-                        longest_ending_at[index] = longest_ending_at[other_number_index] + 1
-        
+                    longest_ending_at[index] = max(
+                        longest_ending_at[index],
+                        longest_ending_at[other_number_index] + 1,
+                    )
+
         return max(longest_ending_at)
+
 
 if __name__ == "__main__":
     solution = LongestIncreasingSubsequence()
--- longest_palindromic_substring/solution.py
+++ longest_palindromic_substring/solution.py
@@ -5,32 +5,32 @@ class LongestPalindromicSubstring:
             while starting_index < ending_index:
                 if string[starting_index] != string[ending_index]:
                     return False
-                
+
                 starting_index += 1
                 ending_index -= 1
-            
+
             return True
-        
+
         longest = (0, 0)
         for i in range(len(string)):
             for j in range(i + 1, len(string)):
                 if is_palindrome(i, j) and j - i > longest[1] - longest[0]:
                     longest = (i, j)
-        
+
         return string[longest[0] : longest[1] + 1]
-    
+
     @staticmethod
     def dp_1(string: str) -> str:
         # The helper functions
         def palindrome_pad(s: str) -> str:
-            assert '!' not in s
-            return '!'.join(s)
+            assert "!" not in s
+            return "!".join(s)
 
         def palindrome_unpad(padded: str) -> str:
-            return padded.replace('!', '')
-        
+            return padded.replace("!", "")
+
         string = palindrome_pad(string)
-        longest_palindrome = "" 
+        longest_palindrome = ""
 
         for center in range(len(string)):
             left, right = center, center
@@ -38,21 +38,20 @@ class LongestPalindromicSubstring:
             while left >= 0 and right < len(string) and string[left] == string[right]:
                 left -= 1
                 right += 1
-            
+
             left += 1
             right -= 1
 
-            possible_solution = palindrome_unpad(string[left: right + 1])
-            
+            possible_solution = palindrome_unpad(string[left : right + 1])
+
             if len(possible_solution) > len(longest_palindrome):
                 longest_palindrome = possible_solution
-        
+
         return longest_palindrome
-    
+
     @staticmethod
     def top_down():
         pass
-
 
 
 if __name__ == "__main__":
